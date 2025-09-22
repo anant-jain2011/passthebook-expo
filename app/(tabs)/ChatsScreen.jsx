b52e5badda8d5d30acae5b17d20673017b249e7f
Anant Jain <110173387+anant-jain2011@users.noreply.github.com>
2025-09-22 14:25:37 +0000

    commit 22 sep

---
+++ app/(tabs)/ChatsScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ChatsScreen() {
+  return (
+    <View className="flex-1 bg-red-300 items-center justify-center">
+      <Text className="font-bold">Chats Screen</Text>
+    </View>
+  );
+}
